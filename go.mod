97ed437d9513320433faf13bdbcc4919162e3dc2
gurgen11 <gurgen@retailmerchantgroup.com>
2022-05-18 23:48:51 +0400

    Reverse String

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/gurgen10/golang-united-school-homework-4.1
+
+go 1.17
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	for _, s := range input {
+		output = string(s) + output
+	}
+
 	return output
 }
